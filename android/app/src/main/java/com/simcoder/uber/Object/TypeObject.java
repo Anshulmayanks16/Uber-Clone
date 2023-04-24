26e7cf358dd6283cc51b83ffedfcb62db0c58017
Anshul  Rajput <94006502+Anshulmayanks16@users.noreply.github.com>
2023-04-24 18:25:39 +0530

    Create TypeObject.java

---
+++ android/app/src/main/java/com/simcoder/uber/Object/TypeObject.java
@@ -0,0 +1,36 @@
+package com.simcoder.uber.Objects;
+
+import android.graphics.drawable.Drawable;
+
+
+
+public class TypeObject {
+
+    String name, id;
+    Drawable image;
+    int people;
+
+   
+    public TypeObject(String id, String name, Drawable image, int people){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.people = people;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPeople() {
+        return people;
+    }
+}
